e6589b0fe7abc8d3b88cae155fcd911e0a095f90
Black Nguyen <blackng10.5.1997@gmail.com>
2021-10-12 08:50:49 +0700

    update

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,22 +1,20 @@
-import { config, createSchema } from "@keystone-next/keystone/schema";
 import { createAuth } from "@keystone-next/auth";
+import { config, createSchema } from "@keystone-next/keystone/schema";
 import {
   withItemData,
   statelessSessions,
 } from "@keystone-next/keystone/session";
-
-import { User } from "./schemas/User";
 import { ProductImage } from "./schemas/ProductImage";
 import { Product } from "./schemas/Product";
-
+import { User } from "./schemas/User";
 import "dotenv/config";
 import { insertSeedData } from "./seed-data";
 
 const databaseURL =
-  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits";
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
 
 const sessionConfig = {
-  maxAge: 60 * 60 * 24 * 360,
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET,
 };
 
@@ -26,11 +24,18 @@ const { withAuth } = createAuth({
   secretField: "password",
   initFirstItem: {
     fields: ["name", "email", "password"],
+    // TODO: Add in inital roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      console.log(args);
+    },
   },
 });
 
 export default withAuth(
   config({
+    // @ts-ignore
     server: {
       cors: {
         origin: [process.env.FRONTEND_URL],
@@ -41,25 +46,27 @@ export default withAuth(
       adapter: "mongoose",
       url: databaseURL,
       async onConnect(keystone) {
-        console.log("Connect to the database");
+        console.log("Connected to the database!");
         if (process.argv.includes("--seed-data")) {
           await insertSeedData(keystone);
         }
       },
     },
     lists: createSchema({
+      // Schema items go in here
       User,
       Product,
       ProductImage,
     }),
     ui: {
-      // Show the UI for people who pass this test
-      isAccessAllowed: ({ session }) => {
-        return !!session?.data;
-      },
+      // Show the UI only for poeple who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
     },
     session: withItemData(statelessSessions(sessionConfig), {
-      User: `id name email`,
+      // GraphQL Query
+      User: "id name email",
     }),
   })
 );
